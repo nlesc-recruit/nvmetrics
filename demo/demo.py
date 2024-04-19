9f4710f3eac178d338e1712ee5d4600f3509c001
Bram Veenboer <bram.veenboer@gmail.com>
2024-04-19 09:25:05 +0200

    Update python demo

--- demo/demo.py
+++ demo/demo.py
@@ -1,17 +1,12 @@
 import nvmetrics
 
-metrics = [
-    "sm__sass_thread_inst_executed_op_fadd_pred_on.sum",
-    "sm__sass_thread_inst_executed_op_fmul_pred_on.sum",
-    "sm__sass_thread_inst_executed_op_ffma_pred_on.sum"
-]
 
 import numpy as np
 import pycuda.autoinit
 import pycuda.driver as cuda
 from pycuda.compiler import SourceModule
 
-mod = SourceModule("""
+module = SourceModule("""
 __global__ void vector_add(float *a, float *b, float *c, int n, int scale)
 {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
@@ -20,32 +15,54 @@ __global__ void vector_add(float *a, float *b, float *c, int n, int scale)
 }
 """)
 
-vector_add = mod.get_function("vector_add")
+if __name__ == "__main__":
+    n = 1024
+    scale = 2.0
 
-n = 1024
+    # Allocate memory on host
+    a = np.random.randn(n).astype(np.float32)
+    b = np.random.randn(n).astype(np.float32)
+    c = np.zeros_like(a)
 
-a = np.random.randn(n).astype(np.float32)
-b = np.random.randn(n).astype(np.float32)
+    # Allocate memory on device
+    a_gpu = cuda.mem_alloc(a.nbytes)
+    b_gpu = cuda.mem_alloc(b.nbytes)
+    c_gpu = cuda.mem_alloc(b.nbytes)
 
-a_gpu = cuda.mem_alloc(a.nbytes)
-b_gpu = cuda.mem_alloc(b.nbytes)
-c_gpu = cuda.mem_alloc(b.nbytes)
+    cuda.memcpy_htod(a_gpu, a)
+    cuda.memcpy_htod(b_gpu, b)
 
-cuda.memcpy_htod(a_gpu, a)
-cuda.memcpy_htod(b_gpu, b)
+    # Launch configuration
+    block_size = 256
+    grid_size = (n + block_size - 1) // block_size
 
-block_size = 256
-grid_size = (n + block_size - 1) // block_size
+    # Setup metrics
+    metrics = [
+        "sm__sass_thread_inst_executed_op_fadd_pred_on.sum",
+        "sm__sass_thread_inst_executed_op_fmul_pred_on.sum",
+        "sm__sass_thread_inst_executed_op_ffma_pred_on.sum"
+    ]
 
-nvmetrics.measureMetricsStart(metrics)
+    # Load kernel
+    vector_add = module.get_function("vector_add")
 
-scale = 2.0
-vector_add(a_gpu, b_gpu, c_gpu, np.int32(n), np.int32(2), block=(block_size, 1, 1), grid=(grid_size, 1))
+    # Start measurement
+    nvmetrics.measureMetricsStart(metrics)
 
-metrics_results = nvmetrics.measureMetricsStop()
+    # Launch kernel
+    vector_add(a_gpu, b_gpu, c_gpu, np.int32(n), np.int32(2), block=(block_size, 1, 1), grid=(grid_size, 1))
 
-for metric, value in zip(metrics, metrics_results):
-    print(f"{metric}: {value}")
+    # Stop measurement
+    results = nvmetrics.measureMetricsStop()
 
-c = np.zeros_like(a)
-cuda.memcpy_dtoh(c, c_gpu)
+    # Print result of the measurement
+    for metric, value in zip(metrics, results):
+        print(f"{metric}: {value}")
+
+    # Copy result back to the host
+    cuda.memcpy_dtoh(c, c_gpu)
+
+    # Free device memory
+    a_gpu.free()
+    b_gpu.free()
+    c_gpu.free()
