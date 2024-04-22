a7c44bed14e2874ac8123ed611f7b2def38b3dfc
Bram Veenboer <bram.veenboer@gmail.com>
2024-04-22 17:44:55 +0200

    Add queryMetrics Python interface

--- python/pynvmetrics.cpp
+++ python/pynvmetrics.cpp
@@ -8,9 +8,19 @@ namespace py = pybind11;
 PYBIND11_MODULE(nvmetrics, m) {
   m.doc() = "Pybind11 interface for nv_metrics library";
 
+  py::enum_<NVPW_MetricType>(m, "NVPW_MetricType")
+      .value("NVPW_METRIC_TYPE_COUNTER", NVPW_METRIC_TYPE_COUNTER)
+      .value("NVPW_METRIC_TYPE_RATIO", NVPW_METRIC_TYPE_RATIO)
+      .value("NVPW_METRIC_TYPE_THROUGHPUT", NVPW_METRIC_TYPE_THROUGHPUT)
+      .export_values();
+
+  m.def("queryMetrics", &nvmetrics::queryMetrics, "Query available metrics",
+        py::arg("metricType"), py::arg("deviceNum") = 0);
+
   m.def("measureMetricsStart", &nvmetrics::measureMetricsStart,
         "Start measuring metrics", py::arg("metricNames"),
         py::arg("deviceNum") = 0);
+        
   m.def("measureMetricsStop", &nvmetrics::measureMetricsStop,
         "Stop measuring metrics");
 }
