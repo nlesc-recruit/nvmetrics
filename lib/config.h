52d62e7737869b2ec48365c6430e6407db8399c3
Bram Veenboer <bram.veenboer@gmail.com>
2024-04-22 11:06:28 +0200

    Update/add docstrings

--- lib/config.h
+++ lib/config.h
@@ -8,16 +8,55 @@
 
 namespace NV::Metric::Config {
 
+/* clang-format off
+ * \brief Retrieves raw metric requests for the specified chip and metric names.
+ *
+ * This function retrieves raw metric requests for the specified chip and metric names
+ * by utilizing NVIDIA's Performance Tools Interface (NVPW) for CUDA Metrics.
+ * Raw metric requests are essential for subsequent metric collection and analysis.
+ *
+ * \param[in]  chipName[in]               The name of the GPU chip for which to get metric values.
+ * \param[in]  metricNames                A list of metrics to read from the counter data image.
+ * \param[out] rawMetricRequests          An output vector where the raw metric requests will be stored.
+ * \param[in]  pCounterAvailabilityImage  A pointer to the counter availability image queried on
+ *                                        the target device.
+ * clang-format on */
 void GetRawMetricRequests(std::string chipName,
                           const std::vector<std::string> &metricNames,
                           std::vector<NVPA_RawMetricRequest> &rawMetricRequests,
                           const uint8_t *pCounterAvailabilityImage);
 
+/* clang-format off
+ * \brief Generates a configuration image for specified GPU chip and metrics.
+ *
+ * This function generates a configuration image for the specified GPU chip and metrics
+ * using NVIDIA's Performance Tools Interface (NVPW) for CUDA Metrics. The configuration
+ * image is crucial for subsequent metric collection and analysis.
+ *
+ * \param[in]  chipName                   The name of the GPU chip for which to get metric values.
+ * \param[in]  metricNames                A list of metrics to read from the counter data image.
+ * \param[out] configImage                An output vector where the configuration image will be stored.
+ * \param[in]  pCounterAvailabilityImage  A pointer to the counter availability image queried on
+ *                                        the target device.
+ * clang-format on */
 void GetConfigImage(std::string chipName,
                     const std::vector<std::string> &metricNames,
                     std::vector<uint8_t> &configImage,
                     const uint8_t *pCounterAvailabilityImage);
 
+/* clang-format off
+ * \brief Retrieves the counter data prefix image for specified GPU chip and metrics.
+ *
+ * This function retrieves the counter data prefix image for the specified GPU chip
+ * and metrics using NVIDIA's Performance Tools Interface (NVPW) for CUDA Metrics.
+ * The counter data prefix image is essential for preparing and interpreting raw counter data.
+ *
+ * \param[in]  chipName                   The name of the GPU chip for which to get metric values.
+ * \param[in]  metricNames                A list of metrics to read from the counter data image.
+ * \param[out] counterDataImagePrefix     An output vector where the counter data prefix image will be stored.
+ * \param[in]  pCounterAvailabilityImage  A pointer to the counter availability image queried on
+ *                                        the target device.
+ * clang-format on */
 void GetCounterDataPrefixImage(std::string chipName,
                                const std::vector<std::string> &metricNames,
                                std::vector<uint8_t> &counterDataImagePrefix,
--- lib/eval.h
+++ lib/eval.h
@@ -6,15 +6,19 @@
 
 namespace NV::Metric::Eval {
 
-/* Function to get aggregate metric values
- * @param[in]  chipName                 Chip name for which to get metric values
- * @param[in]  counterDataImage         Counter data image
- * @param[in]  metricNames              List of metrics to read from counter
- * data image
- * @param[out] metricNameValueMap       Metric name value map
- * @param[in] pCounterAvailabilityImage  Pointer to counter availability image
- * queried on target device
- */
+/* clang-format off
+ * \brief Function to get aggregate metric values.
+ *
+ * This function retrieves aggregate metric values for the specified chip and metrics
+ * using the provided counter data image and counter availability image.
+ *
+ * \param[in]  chipName                   The name of the GPU chip for which to get metric values.
+ * \param[in]  counterDataImage           The counter data image containing raw counter values.
+ * \param[in]  metricNames                A list of metrics to read from the counter data image.
+ * \param[out] metricNameValueMap         A map where the metric values will be stored.
+ * \param[in]  pCounterAvailabilityImage  A pointer to the counter availability image queried on
+ *                                        the target device.
+ * clang-format on */
 std::vector<double>
 GetMetricValues(std::string chipName,
                 const std::vector<uint8_t> &counterDataImage,
