4286351141425632a28b6dc54ad64d7afedfaf9c
Bram Veenboer <bram.veenboer@gmail.com>
2024-04-22 11:16:43 +0200

    Split utils in header and source file

--- lib/utils.h
+++ lib/utils.h
@@ -9,131 +9,20 @@
 #include <nvperf_host.h>
 namespace NV::Metric::Utils {
 
-static const char *nvpwGetErrorMessage(NVPA_Status status) {
-  const char *errorMsg = NULL;
-  switch (status) {
-  case NVPA_STATUS_ERROR:
-    errorMsg = "NVPA_STATUS_ERROR";
-    break;
-  case NVPA_STATUS_INTERNAL_ERROR:
-    errorMsg = "NVPA_STATUS_INTERNAL_ERROR";
-    break;
-  case NVPA_STATUS_NOT_INITIALIZED:
-    errorMsg = "NVPA_STATUS_NOT_INITIALIZED";
-    break;
-  case NVPA_STATUS_NOT_LOADED:
-    errorMsg = "NVPA_STATUS_NOT_LOADED";
-    break;
-  case NVPA_STATUS_FUNCTION_NOT_FOUND:
-    errorMsg = "NVPA_STATUS_FUNCTION_NOT_FOUND";
-    break;
-  case NVPA_STATUS_NOT_SUPPORTED:
-    errorMsg = "NVPA_STATUS_NOT_SUPPORTED";
-    break;
-  case NVPA_STATUS_NOT_IMPLEMENTED:
-    errorMsg = "NVPA_STATUS_NOT_IMPLEMENTED";
-    break;
-  case NVPA_STATUS_INVALID_ARGUMENT:
-    errorMsg = "NVPA_STATUS_INVALID_ARGUMENT";
-    break;
-  case NVPA_STATUS_INVALID_METRIC_ID:
-    errorMsg = "NVPA_STATUS_INVALID_METRIC_ID";
-    break;
-  case NVPA_STATUS_DRIVER_NOT_LOADED:
-    errorMsg = "NVPA_STATUS_DRIVER_NOT_LOADED";
-    break;
-  case NVPA_STATUS_OUT_OF_MEMORY:
-    errorMsg = "NVPA_STATUS_OUT_OF_MEMORY";
-    break;
-  case NVPA_STATUS_INVALID_THREAD_STATE:
-    errorMsg = "NVPA_STATUS_INVALID_THREAD_STATE";
-    break;
-  case NVPA_STATUS_FAILED_CONTEXT_ALLOC:
-    errorMsg = "NVPA_STATUS_FAILED_CONTEXT_ALLOC";
-    break;
-  case NVPA_STATUS_UNSUPPORTED_GPU:
-    errorMsg = "NVPA_STATUS_UNSUPPORTED_GPU";
-    break;
-  case NVPA_STATUS_INSUFFICIENT_DRIVER_VERSION:
-    errorMsg = "NVPA_STATUS_INSUFFICIENT_DRIVER_VERSION";
-    break;
-  case NVPA_STATUS_OBJECT_NOT_REGISTERED:
-    errorMsg = "NVPA_STATUS_OBJECT_NOT_REGISTERED";
-    break;
-  case NVPA_STATUS_INSUFFICIENT_PRIVILEGE:
-    errorMsg = "NVPA_STATUS_INSUFFICIENT_PRIVILEGE";
-    break;
-  case NVPA_STATUS_INVALID_CONTEXT_STATE:
-    errorMsg = "NVPA_STATUS_INVALID_CONTEXT_STATE";
-    break;
-  case NVPA_STATUS_INVALID_OBJECT_STATE:
-    errorMsg = "NVPA_STATUS_INVALID_OBJECT_STATE";
-    break;
-  case NVPA_STATUS_RESOURCE_UNAVAILABLE:
-    errorMsg = "NVPA_STATUS_RESOURCE_UNAVAILABLE";
-    break;
-  case NVPA_STATUS_DRIVER_LOADED_TOO_LATE:
-    errorMsg = "NVPA_STATUS_DRIVER_LOADED_TOO_LATE";
-    break;
-  case NVPA_STATUS_INSUFFICIENT_SPACE:
-    errorMsg = "NVPA_STATUS_INSUFFICIENT_SPACE";
-    break;
-  case NVPA_STATUS_OBJECT_MISMATCH:
-    errorMsg = "NVPA_STATUS_OBJECT_MISMATCH";
-    break;
-  case NVPA_STATUS_VIRTUALIZED_DEVICE_NOT_SUPPORTED:
-    errorMsg = "NVPA_STATUS_VIRTUALIZED_DEVICE_NOT_SUPPORTED";
-    break;
-  default:
-    break;
-  }
+void THROW_RUNTIME_ERROR(const char *apiFuncCall, const char *message,
+                         const char *file, int line);
 
-  return errorMsg;
-}
+void checkNVPWError(NVPA_Status status, const char *apiFuncCall,
+                    const char *file, int line);
 
-inline void THROW_RUNTIME_ERROR(const char *apiFuncCall, const char *message,
-                                const char *file, int line) {
-  std::stringstream errorMsg;
-  errorMsg << file << ":" << line << ": error: function " << apiFuncCall
-           << " failed with error " << message << ".\n";
-  throw std::runtime_error(errorMsg.str());
-}
+void checkCUPTIError(CUptiResult status, const char *apiFuncCall,
+                     const char *file, int line);
 
-inline void checkNVPWError(NVPA_Status status, const char *apiFuncCall,
-                           const char *file, int line) {
-  if (status != NVPA_STATUS_SUCCESS) {
-    const char *message = NV::Metric::Utils::nvpwGetErrorMessage(status);
-    THROW_RUNTIME_ERROR(apiFuncCall, message, file, line);
-  }
-}
+void checkDriverError(CUresult status, const char *apiFuncCall,
+                      const char *file, int line);
 
-inline void checkCUPTIError(CUptiResult status, const char *apiFuncCall,
-                            const char *file, int line) {
-  if (status != CUPTI_SUCCESS) {
-    const char *message = nullptr;
-    cuptiGetErrorMessage(status, &message);
-    THROW_RUNTIME_ERROR(apiFuncCall, message, file, line);
-  }
-}
-
-inline void checkDriverError(CUresult status, const char *apiFuncCall,
-                             const char *file, int line) {
-  if (status != CUDA_SUCCESS) {
-    const char *message = nullptr;
-    cuGetErrorString(status, &message);
-    THROW_RUNTIME_ERROR(apiFuncCall, message, file, line);
-  }
-}
-
-inline void checkRuntimeError(cudaError_t status, const char *apiFuncCall,
-                              const char *file, int line) {
-  if (status != cudaSuccess) {
-    const char *message = cudaGetErrorString(status);
-    THROW_RUNTIME_ERROR(apiFuncCall, message, file, line);
-  }
-}
-
-} // namespace NV::Metric::Utils
+void checkRuntimeError(cudaError_t status, const char *apiFuncCall,
+                       const char *file, int line);
 
 #define NVPW_API_CALL(apiFuncCall)                                             \
   NV::Metric::Utils::checkNVPWError(apiFuncCall, #apiFuncCall, __FILE__,       \
@@ -147,5 +36,7 @@ inline void checkRuntimeError(cudaError_t status, const char *apiFuncCall,
 #define RUNTIME_API_CALL(apiFuncCall)                                          \
   NV::Metric::Utils::checkRuntimeError(apiFuncCall, #apiFuncCall, __FILE__,    \
                                        __LINE__)
+
+} // namespace NV::Metric::Utils
 
 #endif // NV_METRIC_UTILS_H_
