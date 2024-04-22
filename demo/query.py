170171ec5b12e91e965b7dbac6e3de534d35b829
Bram Veenboer <bram.veenboer@gmail.com>
2024-04-22 17:45:12 +0200

    Add demo for Python interface queryMetrics

---
+++ demo/query.py
@@ -0,0 +1,15 @@
+import nvmetrics
+
+import pycuda.autoinit
+
+if __name__ == "__main__":
+    def print_metrics(metric_type, metrics):
+        print("#" * 80)
+        print(f"# {len(metrics)} {metric_type} metrics")
+        print("#" * 80)
+        print("\n".join(metrics))
+        print()
+
+    print_metrics("COUNTER", nvmetrics.queryMetrics(nvmetrics.NVPW_METRIC_TYPE_COUNTER))
+    print_metrics("RATIO", nvmetrics.queryMetrics(nvmetrics.NVPW_METRIC_TYPE_RATIO))
+    print_metrics("THROUGHPUT", nvmetrics.queryMetrics(nvmetrics.NVPW_METRIC_TYPE_THROUGHPUT))
