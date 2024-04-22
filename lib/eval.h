8ed0fc63fee366bf6b1a2eb39953676c17d45bb8
Bram Veenboer <bram.veenboer@gmail.com>
2024-04-22 10:29:29 +0200

    Remove unused GetMetricGpuValue function

--- lib/eval.h
+++ lib/eval.h
@@ -11,21 +11,6 @@ struct MetricNameValue {
   std::vector<std::pair<std::string, double>> rangeNameMetricValueMap;
 };
 
-/* Function to get aggregate metric values
- * @param[in]  chipName                 Chip name for which to get metric values
- * @param[in]  counterDataImage         Counter data image
- * @param[in]  metricNames              List of metrics to read from counter
- * data image
- * @param[out] metricNameValueMap       Metric name value map
- * @param[in] pCounterAvailabilityImage  Pointer to counter availability image
- * queried on target device
- */
-bool GetMetricGpuValue(std::string chipName,
-                       const std::vector<uint8_t> &counterDataImage,
-                       const std::vector<std::string> &metricNames,
-                       std::vector<MetricNameValue> &metricNameValueMap,
-                       const uint8_t *pCounterAvailabilityImage = NULL);
-
 /* Function to print aggregate metric values
  * @param[in]   chipName                    Chip name for which to get metric
  * values
@@ -40,6 +25,15 @@ bool PrintMetricValues(std::string chipName,
                        const std::vector<std::string> &metricNames,
                        const uint8_t *pCounterAvailabilityImage = NULL);
 
+/* Function to get aggregate metric values
+ * @param[in]  chipName                 Chip name for which to get metric values
+ * @param[in]  counterDataImage         Counter data image
+ * @param[in]  metricNames              List of metrics to read from counter
+ * data image
+ * @param[out] metricNameValueMap       Metric name value map
+ * @param[in] pCounterAvailabilityImage  Pointer to counter availability image
+ * queried on target device
+ */
 std::vector<double>
 GetMetricValues(std::string chipName,
                 const std::vector<uint8_t> &counterDataImage,
